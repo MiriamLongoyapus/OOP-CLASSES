b8727e02c021e0227b4f8741f6ce7ae5e7731d9c
Miriam <longoyapusmiriam@gmail.com>
2023-06-27 10:39:39 +0300

    first commit

---
+++ python.py
@@ -0,0 +1,10 @@
+# // 1. **Ancestral Stories:** In many African cultures, the art of storytelling is passed
+# // down from generation to generation. Imagine you're creating an application that
+# // records these oral stories and translates them into different languages. The
+# // stories vary by length, moral lessons, and the age group they are intended for.
+# // Think about how you would model `Story`, `StoryTeller`, and `Translator`
+# // objects, and how inheritance might come into play if there are different types of
+# // stories or storytellers.
+# // pseudo code
+# // 1. record stories as objects(length,moral lessons,age-group)
+# // 2.Create another class with more attributes for storyteller and translato
